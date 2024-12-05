6d56070079da73e58e3e8360660fb4c1f7dd315d
Mustafee <mussemoh03@gmail.com>
2024-12-05 08:41:03 +0100

    Add project files.

---
+++ Animals-Linq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Animals_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
